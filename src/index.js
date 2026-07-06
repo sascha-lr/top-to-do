95a004ac2d78816dd4acff11e854a9927998e5de
sascha-lr <174216621+sascha-lr@users.noreply.github.com>
2026-07-06 12:23:49 +0200

    Add labels, write basic task creation testing func

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./style.css";
 import { mainController } from "./elements/mainController.js";
+
+const contentContainer = document.querySelector('[data-label="content-container"]');
+const emptyContent = document.querySelector('[data-label="empty-content"]');
+const populatedContent = document.querySelector('[data-label="populated-content"]');
+
+contentContainer.addEventListener('click', (e) => {
+    if (e.target.closest('[data-action="add-task"]')) {
+        emptyContent.classList.remove('active');
+        populatedContent.classList.add('active');
+        mainController.makeTask('Hello');
+    }
+})
