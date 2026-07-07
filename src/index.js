3e66cd4152832bd4c2921397ade6aae7f42b7a1e
sascha-lr <174216621+sascha-lr@users.noreply.github.com>
2026-07-07 15:07:00 +0200

    Add basic task creation logic

--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ const contentContainer = document.querySelector('[data-label="content-container"
 const emptyContent = document.querySelector('[data-label="empty-content"]');
 const populatedContent = document.querySelector('[data-label="populated-content"]');
 const dateInput = document.querySelector('#task-creation-dialog input[type="datetime-local"]');
+const form = document.querySelector('#task-creation-dialog form');
 
 contentContainer.addEventListener('click', (e) => {
     if (e.target.closest('[data-action="add-task"]')) {
-        emptyContent.classList.remove('active');
-        populatedContent.classList.add('active');
         dateInput.value = new Date().toISOString().split('T')[0] + 'T23:59';
     }
+})
+
+form.addEventListener('submit', () => {
+    const formData = new FormData(form);
+    const taskName = formData.get('task-name');
+    const taskDesc = formData.get('task-desc');
+    const taskDueDate = formData.get('task-due-date');
+    const taskPriority = formData.get('task-priority');
+
+    emptyContent.classList.remove('active');
+    populatedContent.classList.add('active');
+    mainController.makeTask(taskName, taskDesc, taskDueDate, taskPriority);
+
+    form.reset();
 })
